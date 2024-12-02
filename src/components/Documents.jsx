ad7477c9591f0e532b2ac3e125094696823cda56
Cupadilikabra <endis.l@tvnet.lv>
2024-12-02 17:03:27 +0200

    changes 2

--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -5,9 +5,9 @@ function Documents(){
        <div className="container">
         <h1>Documents & CV</h1>
 <ul className="column-list">
-    <li><a  href="./Ter-portfolio/endijs_lacis_cv_eng.pdf" target="_blank" rel="noopener noreferrer"> View CV </a></li>
+    <li><a  href="/endijs_lacis_cv_eng.pdf" target="_blank" rel="noopener noreferrer"> View CV </a></li>
     <li><a  href="https://www.sololearn.com/en/certificates/CC-MOI7ZM0T" target="_blank" rel="noopener noreferrer">Sololearn JS Cer</a> </li>
-    <li><a  href="./Ter-portfolio/RCS-Cer.pdf" target="_blank" rel="noopener noreferrer"> RCS Certivicate </a></li>
+    <li><a  href="/RCS-Cer.pdf" target="_blank" rel="noopener noreferrer"> RCS Certivicate </a></li>
 
 
 </ul>
